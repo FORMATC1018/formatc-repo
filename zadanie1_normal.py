a203f3eb1afb3f218542ce3744796eb5d864d625
FORMATC1018 <kraskokirill1018@mail.ru>
2018-12-26 22:44:12 +0300

    lesson1

--- zadanie1_normal.py
+++ zadanie1_normal.py
@@ -7,10 +7,12 @@ while True:
 		i+=1
 	if i==1: break
 	else: continue
+max_digit = 0
 if number<0:
 	number = number * (-1)
-
 while number != 0:
 	digit = number % 10
-	number = number // 10
-	print ("Цифра числа - ", digit)
+	number //= 10
+	if digit>max_digit:
+		max_digit = digit
+print ("Максимальная цифра числа - ", max_digit)
---
+++ zadanie3_hard.py
@@ -0,0 +1,5 @@
+import math
+a=math.inf
+print (a==a**2)
+print(a==2*a)
+print(a>999999)
