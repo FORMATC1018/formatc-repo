861a2e14c20dc9875e4f9f6dc11a9b071f99156b
FORMATC1018 <kraskokirill1018@mail.ru>
2019-01-21 13:18:49 +0300

    Revert "lesson3 easy normal"
    This reverts commit 8f39ce0cd6629c16c90456451b229d492a7574d3.

--- hw3_normal_3.py
+++
@@ -1,9 +0,0 @@
-def filter_func(function, iterable):
-    return (item for item in iterable if function(item))
- 
-import random
-N = int(input("Введите количество элементов в списке - "))
-lst = [random.randint(-100, 100) for i in range(N)]
-print ("Исходный список " , lst)
-print("Отфильтрованный список (четные положительные числа - )", list(filter_func(lambda x: True if x % 2 == 0 and x>0 else False,
-                  lst)))
