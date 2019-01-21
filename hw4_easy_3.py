3ab18d65f8adf884096803ce192ce11e0fe32a01
FORMATC1018 <kraskokirill1018@mail.ru>
2019-01-21 13:18:48 +0300

    Revert "lesson4"
    This reverts commit 43d922a5711de8ad429aff8f6402b7432d6fedb2.

--- hw4_easy_3.py
+++
@@ -1,9 +0,0 @@
-import random
-#Заполняем список случайными числами
-N = int(input("Введите количество элементов в списке - "))
-lst = [random.randint(-100, 100) for i in range(N)]
-#Выводим исходный список
-print("Исходный список - ", lst)
-#Генерируем новый список и выводим его
-res_lst = [x for x in lst if x>0 and x%3==0 and x%4!=0]
-print("Новый список - ", res_lst)
